c0dda50eb44ccd98242267b4c0f2b26b3ee25c53
oanagrecu <oanagrecu09@yahoo.com>
2023-09-19 16:58:25 +0200

    adding the structure of the heroSection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.png';
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Homescreen";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I`m Oana.</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">FrontEnd </span> <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            <br />
+            Possimus cupiditate rerum, culpa dolores saepe repudiandae! Iste
+            itaque natus officia quod.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get in Touch!</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/Oana_Grecu.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
