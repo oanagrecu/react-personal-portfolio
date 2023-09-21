2ec0ce6c127fb91d6dd1ad5af31fa81418eb1248
oanagrecu <oanagrecu09@yahoo.com>
2023-09-21 16:55:33 +0200

    adding portfolioPage and some style

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,13 +1,14 @@
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
 import AboutMe from "../AboutMe";
-
+import MyPortfolio from "../MyPortfolio";
 export default function Home() {
   return (
     <>
       <HeroSection />
       <MySkills />
       <AboutMe />
+      <MyPortfolio />
     </>
   );
 }
