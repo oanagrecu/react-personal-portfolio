8a9128d46380bca10753cad7bd196bf3cf6d17e5
oanagrecu <oanagrecu09@yahoo.com>
2023-09-21 15:21:55 +0200

    adding pictures

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -16,9 +16,8 @@ export default function HeroSection() {
         <button className="btn btn-primary">Get In Touch</button>
       </div>
       <div className="hero--section--img">
-        <img src="./img/hero_img.png" alt="Hero Section" />
+        <img src="./img/cardOana.png" alt="Hero Section" />
       </div>
     </section>
-    
   );
 }
